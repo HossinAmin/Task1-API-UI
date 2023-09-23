b8f69af20caf5992245af4600d9072c189545dfc
HossinAmin <hossinamin@diverse.dev>
2023-09-23 20:22:54 +0300

    project init + packages

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-teal-100 ">
+      <Button>hello ant design</Button>
+    </div>
+  );
+}
+
+export default App;
