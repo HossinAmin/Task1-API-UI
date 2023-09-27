5eb54e0523ea8631c04fd8f76905730d2035db82
HossinAmin <hossinamin@diverse.dev>
2023-09-27 16:07:15 +0300

    additional form Ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,29 @@ import ApplicationSteps from "./components/ApplicationSteps";
 import UploadDragger from "./components/UploadDragger";
 import useAppFormData from "./hooks/useAppFormData";
 import { useEffect } from "react";
+import PersonalInfoForm from "./components/forms/personalInfoForm";
+import ProfileForm from "./components/forms/ProfileForm";
+import AdditionalForm from "./components/forms/AdditionalForm";
 
 function App() {
-  const { fetchAppFormData, appFormData } = useAppFormData();
+  // const { fetchAppFormData, appFormData } = useAppFormData();
 
-  // inital fetch
-  useEffect(() => {
-    fetchAppFormData();
-  }, [fetchAppFormData]);
-  console.log(appFormData);
+  // // inital fetch
+  // useEffect(() => {
+  //   fetchAppFormData();
+  // }, [fetchAppFormData]);
+  // console.log(appFormData);
 
   return (
     <div className="flex min-h-screen bg-white">
       <Sidebar />
       <main className="flex flex-grow flex-col gap-5 py-20">
         <ApplicationSteps />
-        <div className="flex w-full flex-col bg-white p-10">
+        <div className="flex w-full flex-col gap-16 bg-white p-10">
           <UploadDragger />
+          <PersonalInfoForm />
+          <ProfileForm />
+          <AdditionalForm />
         </div>
       </main>
     </div>
