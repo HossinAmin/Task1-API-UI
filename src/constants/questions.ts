6ea83bea38f3250654e7d619a2af5d5b9a5f59be
HossinAmin <hossinamin@diverse.dev>
2023-09-28 00:40:40 +0300

    types of question a constant

---
+++ src/constants/questions.ts
@@ -0,0 +1,11 @@
+export const QuestionTypes = {
+  Paragraph: "Paragraph",
+  ShortAnswer: "ShortAnswer",
+  YesNo: "YesNo",
+  Dropdown: "Dropdown",
+  MultipleChoice: "MultipleChoice",
+  Date: "Date",
+  Number: "Number",
+  FileUpload: "FileUpload",
+  VideoQuestion: "VideoQuestion",
+};
