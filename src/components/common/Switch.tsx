217d6446db41cb4d863c44286a92488befb66055
HossinAmin <hossinamin@diverse.dev>
2023-09-27 16:02:21 +0300

    edit switch styles to match design

--- src/components/common/Switch.tsx
+++ src/components/common/Switch.tsx
@@ -1,16 +1,11 @@
 import React, { useState } from "react";
 
 interface SwitchProps {
-  label: string;
   onChange?: (checked: boolean) => void;
   checked?: boolean;
 }
 
-const Switch: React.FC<SwitchProps> = ({
-  label,
-  onChange,
-  checked = false,
-}) => {
+const Switch: React.FC<SwitchProps> = ({ onChange, checked = false }) => {
   const [isChecked, setIsChecked] = useState(checked);
 
   const toggleSwitch = () => {
@@ -22,19 +17,19 @@ const Switch: React.FC<SwitchProps> = ({
   return (
     <div className="flex items-center ">
       <div
-        className={`relative flex h-4 w-8 items-center rounded-full transition duration-300 ease-in-out ${
-          isChecked ? "bg-blue-500" : "bg-gray-300"
+        className={`relative flex h-4 w-8 items-center justify-end rounded-full transition duration-300 ease-in-out ${
+          isChecked ? "bg-[#087B2F]" : "bg-gray-300"
         }`}
         onClick={toggleSwitch}
       >
         <div
           className={`absolute h-3 w-3 transform rounded-full bg-white shadow-md transition duration-300 ${
-            isChecked ? "translate-x-4" : ""
+            isChecked ? "-translate-x-4" : ""
           }`}
-          style={{ margin: "2px" }} // Add a small margin to the circle
+          style={{ margin: "2px" }}
         />
       </div>
-      <label className="ml-2">{label}</label>
+      <label className="ml-2 w-8">{isChecked ? "Show" : "Hide"}</label>
     </div>
   );
 };
